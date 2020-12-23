f8c0a18b37a27202c9cc129fcc2a6d666e3aef06
xzackli <xzackli@gmail.com>
2020-12-22 19:03:59 -0500

    seems to work

--- README.md
+++ README.md
@@ -4,3 +4,24 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://xzackli.github.io/StaticThreadSchedules.jl/dev)
 [![Build Status](https://github.com/xzackli/StaticThreadSchedules.jl/workflows/CI/badge.svg)](https://github.com/xzackli/StaticThreadSchedules.jl/actions)
 [![Coverage](https://codecov.io/gh/xzackli/StaticThreadSchedules.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/xzackli/StaticThreadSchedules.jl)
+
+This package extends `Threads.@threads` static scheduling to handle loops with different costs per iteration.
+
+## Usage
+```julia
+using StaticThreadSchedules
+
+v = zeros(12)
+# suppose this operation scales like log(i)
+@threads :static (i->log(i+1)) for i in 1:12
+    v[i] = Threads.threadid()
+end
+print(v)
+```
+```
+[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
+```
+
+Sometimes when you're computing something expensive, you might already know how the cost of a loop scales with iteration. For example, if you're filling in the upper triangle of a matrix by iterating over the rows and columns, then the number of relevant elements in a column scales linearly.
+
+This package exports `@threads :static [cost function]`, allowing one to specify the cost of loop iteration when passed an index. For example, mode-coupling matrices in CMB studies cost ~ `ℓ^3`, so each iteration of the outer loop costs ℓ^2. Then you can write `@threads :static (ℓ->ℓ^2) for ...` to balance the cost over threads evenly.
--- src/StaticThreadSchedules.jl
+++ src/StaticThreadSchedules.jl
@@ -1,5 +1,139 @@
 module StaticThreadSchedules
 
-# Write your package code here.
+import Base.Threads: threadid, threading_run, nthreads, _threadsfor
+export @threads
+
+
+function _threadsfor(iter, lbody, schedule, cost_model)
+    lidx = iter.args[1]         # index
+    range = iter.args[2]
+
+    quote
+        local threadsfor_fun
+        let range = $(esc(range))
+
+        # compute the normalization of the cost
+        per_thread_cost = 0.0
+        for i in range
+            per_thread_cost += Float64($(esc(cost_model))(i))
+        end
+        per_thread_cost /= nthreads()
+
+        function threadsfor_fun(onethread=false)
+            r = range  # Load into local variable
+            cost = per_thread_cost  # Load into local variable
+
+            # divide loop iterations among threads
+            if onethread
+                tid = 1
+                f = firstindex(r)
+                l = lastindex(r)
+            else
+                tid = threadid()
+
+                # cumulative cost start and end, for this thread
+                first_cost = (tid - 1) * cost
+                last_cost = first_cost + cost
+
+                # compute this thread's iterations
+                f = firstindex(r)
+                cumulative_cost = 0.0
+                for i = r
+                    cumulative_cost += Float64($(esc(cost_model))(i))
+                    if cumulative_cost > first_cost
+                        f = i
+                        break  # stop iterating when we pass the first cost we want
+                    end
+                end
+                l = f
+                for i = f:lastindex(r)
+                    cumulative_cost += Float64($(esc(cost_model))(i))
+                    if cumulative_cost > last_cost
+                        l = i
+                        break  # stop iterating when we pass the last cost we want
+                    end
+                end
+            end
+
+            # run this thread's iterations
+            for i = f:l
+                local $(esc(lidx)) = @inbounds r[i]
+                $(esc(lbody))
+            end
+        end
+        end
+        if threadid() != 1 || ccall(:jl_in_threaded_region, Cint, ()) != 0
+            $(if schedule === :static
+              :(error("`@threads :static` can only be used from thread 1 and not nested"))
+              else
+              # only use threads when called from thread 1, outside @threads
+              :(Base.invokelatest(threadsfor_fun, true))
+              end)
+        else
+            threading_run(threadsfor_fun)
+        end
+        nothing
+    end
+end
+
+"""
+    StaticThreadSchedules.@threads [schedule] [optional scaling] for ... end
+
+A macro to parallelize a `for` loop to run with multiple threads. Splits the iteration
+space among multiple tasks and runs those tasks on threads according to a scheduling
+policy.
+A barrier is placed at the end of the loop which waits for all tasks to finish
+execution.
+
+The `schedule` argument can be used to request a particular scheduling policy.
+If one specifies `:static` and a cost function, then the work will be split such that the cost
+is approximately equal per thread. Specifying `:static` is an error
+if used from inside another `@threads` loop or from a thread other than 1.
+
+The default schedule (used when no `schedule` argument is present) is subject to change.
+
+!!! compat "Julia 1.5"
+    The `schedule` argument is available as of Julia 1.5.
+"""
+macro threads(args...)
+    na = length(args)
+    if na == 3
+        sched, cost_model, ex = args
+        if sched isa QuoteNode
+            sched = sched.value
+        elseif sched isa Symbol
+            # for now only allow quoted symbols
+            sched = nothing
+        end
+        if sched !== :static
+            throw(ArgumentError("unsupported schedule argument in @threads"))
+        end
+        return _threadsfor(ex.args[1], ex.args[2], sched, cost_model)
+    elseif na == 2
+        sched, ex = args
+        if sched isa QuoteNode
+            sched = sched.value
+        elseif sched isa Symbol
+            # for now only allow quoted symbols
+            sched = nothing
+        end
+        if sched !== :static
+            throw(ArgumentError("unsupported schedule argument in @threads"))
+        end
+    elseif na == 1
+        sched = :default
+        ex = args[1]
+    else
+        throw(ArgumentError("wrong number of arguments in @threads"))
+    end
+    if !(isa(ex, Expr) && ex.head === :for)
+        throw(ArgumentError("@threads requires a `for` loop expression"))
+    end
+    if !(ex.args[1] isa Expr && ex.args[1].head === :(=))
+        throw(ArgumentError("nested outer loops are not currently supported by @threads"))
+    end
+    return _threadsfor(ex.args[1], ex.args[2], sched)
+end
+
 
 end
